a651046e78aba9c11fe7d3bcb6447bd0d4e1e973
sanjuel <samueljscott101@gmail.com>
2022-08-29 08:13:46 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,9 +21,45 @@ const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe("arrayShuffler", () => {
+    it("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+        expect(arrayShuffler(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+        expect(arrayShuffler(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+    })
+})
+
 
 // b) Create the function that makes the test pass.
 
+//Pseudo Code
+// Create a function that takes in an array
+//write logic that removes the first index of that array
+//write logic that shuffles the values of an array
+//assign an empty array then push the contents onto it
+//output is the new array with the shuffled values
+
+const arrayShuffler = (array) => {
+array.shift()
+let newArray = []
+ return newArray.push(array.splice(Math.floor(Math.random () * array.length)))
+}
+
+//After doing some research on shuffling arrays, I found out about the fisher yates algorithm, which seems to be a basis for shuffling arrays in JavaScript. Since my original arrayShuffler wasn't working, I decided to try again using that, but I'm going to leave my original one there so you can see my original blocker. I was trying to first use.shift to get rid of the first value in the array, then assign a new array that i can push the rest of the values to. Then, I tried to use the splice method to remove and replace the values at different indexes at random using the math.floor and math.random functions, times the array length so that it always does it the amount of times as the length of the array.
+
+//Here is my next iteration after doing more research.
+
+const arrayShuffler = (array) => {
+    array.shift()
+    for(let i = array.length - 1; i > 0; i--) {
+        let random = Math.floor(Math.random()) * (i + 1);
+        let newArr = array[i]
+        array[i] = array[random];
+        array[random] = newArr;
+        return array
+    }
+}
+
+//From what I can see, the main differences between this and what I was doing is the for loop. Here I'm iterating through the array, taking a random value from the array, then replacing that with whatever is at that index in the original array. I can understand why this works, but I don't think I would've been able to come up with it on my own if not for looking it up and having a reference with the code. 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the net total of votes.
 
@@ -34,13 +70,36 @@ const votes1 = {upVotes: 13, downVotes: 2}
 const votes2 = {upVotes: 2, downVotes: 33}
 // Expected output: -31
 
+describe("allVotes", () => {
+    it("takes in an object that contains up votes and down votes and returns the net total of votes.", () => {
+        expect(allVotes(votes1)).toEqual(11)
+        expect(allVotes(votes2)).toEqual(-31)
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+//Pseudo Code
+// Create a function that takes in an object
+// write logic that subtracts the first key value pair to the second key value pair for the total number of votes
+//output a number with the net total of votes.
+
+const allVotes = (object) => {
+    return object.upVotes - object.downVotes
+}
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("arrayCombiner", () => {
+    it("takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+        expect(arrayCombiner(dataArray1 + dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+        
+    })
+})
+
 
 const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
@@ -48,3 +107,31 @@ const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 
 
 // b) Create the function that makes the test pass.
+
+//Pseudo Code
+//Create a function that takes in two arrays as arguments
+//write logic that combines the two arrays
+//iterate over the array and use conditionals to find out if the value at any given index is the same as another value at a different index
+//output an array with the two arrays combined, with no duplicates. 
+
+const arrayCombiner = (arr1, arr2) => {
+    newArr = arr1.concat(arr2)
+    arrayNoDuplicates = newArr.filter((value, index) => {
+        if (value[i] === value[i-1])
+        return arrayNoDuplicates - value[i]
+    })
+    return arrayNoDuplicates
+}
+
+
+// I'm trying to figure out a way to say "If the value at the index is strictly equal to the value at any other index, delete one of those values, then return the new array." Im getting the error that newArr.filter is not a function, and I'm not sure why that is. Higher order functions have always been a bit of a hard subject for me, so I'm trying to work through them. 
+
+// I tried taking another look at it, and removing the .filter method to use a regular for loop, and came up with this. Now, I'm getting the output of one. I feel like if i can find a way to use.filter this problem will be a lot easier.
+
+const arrayCombiner = (arr1, arr2) => {
+    let newArr = arr1.concat(arr2)
+    let arrayNoDuplicates = []
+    for(let i = 0; i < newArr.length; i++)
+        if(newArr.indexOf(newArr[i])=== i) 
+    return arrayNoDuplicates.push(newArr[i])
+}
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,6 +3,21 @@
 
 # --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
 
+#Pseudo Code
+# First I will create a method that takes in a number
+# I will write logic that determines whether the number is even or odd
+#This will contain conditionals
+# It will return a string with the number and whether it is odd or even.
+# Things I will use: .even? or the modulo operator, conditionals.
+
+def odd_even num
+    if num.even?
+        "#{num} is even"
+else
+    "#{num} is odd"
+end
+end
+
 num1 = 7
 # Expected output: '7 is odd'
 num2 = 42
@@ -10,10 +25,18 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
-
+p odd_even num1
+p odd_even num2
+p odd_even num3
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
 # HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+#Pseudo Code
+#First I will create a method that takes in a string
+#I will write logic that iterates over a string and determines, using conditionals, if the value that corresponds to the index is a vowel.
+#Then it will output the string, but without the vowels.
+#Things I will use: conditionals, .delete, an array with all vowels in it. 
 
 beatles_album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -22,9 +45,23 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def vowel_checker string
+    string.delete "AEIOU, aeiou"
+end
 
+p vowel_checker beatles_album1
+p vowel_checker beatles_album2
+p vowel_checker beatles_album3
+
+# Originally I thought i was going to have to use a for loop, but I think I was still thinking in JavaScript. 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+
+#Pseduo Code
+# First I will create a method that takes in a string
+# It will use conditionals to determine whether the string is equal to itself backwards
+# Then, it will use string interpolation to put out a string that says whether it is a palindrome or not
+#Things I will use: Conditionals, .reverse, equality operators. 
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -32,3 +69,17 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def palindrome string
+    if string.downcase == string.downcase.reverse
+        "#{string} is a palindrome"
+    else 
+        "#{string} is not a palindrome"
+    end
+end
+
+p palindrome palindrome_tester1
+p palindrome palindrome_tester2
+p palindrome palindrome_tester3
+
+# I ran into an issue when first making this code due to the inclusion of capitals, so I had to turn everything into lowercase using the .downcase method. 
